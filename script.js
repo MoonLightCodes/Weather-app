6de83154b8bc8bb6bb356a22102de5db91bf3589
MoonLightCodes <poornachander295@gmail.com>
2024-10-10 14:39:21 +0530

    Weather app
    Weather App
    build on java-script api  and html with all changes done and checked 2

---
+++ script.js
@@ -0,0 +1,65 @@
+const locationRef = document.querySelector('.location');
+const iconRef = document.querySelector('.icon');
+const temperatureRef = document.querySelector('.temperature');
+const timeRef = document.querySelector('.time');
+const textRef = document.querySelector('.text');
+const buttonRef = document.querySelector('.footer button');
+const inputRef = document.querySelector('.footer input');
+const windspdRef = document.querySelector('.wind-speed-val');
+const humidityRef = document.querySelector('.humidity-val');
+const bodydRef = document.querySelector('.body');
+const backgroundRef = document.querySelector('.background');
+
+// by default focus on search bar
+inputRef.focus();
+
+// 1. On click, get location name
+buttonRef.addEventListener('click', (event) => {
+    event.preventDefault();
+    const city = inputRef.value;
+    console.log(city);
+    fetchData(city);
+    backgroundRef.classList.remove('rotate');
+    // backgroundRef.classList.remove('down');
+    // backgroundRef.classList.remove('up');
+} );
+
+inputRef.addEventListener("keypress", event => {
+    if(event.key==='Enter'){
+        event.preventDefault();
+        buttonRef.click();
+    }
+} )
+
+// 2. Make an API call to server to get weather details
+function fetchData(location){
+    fetch(`https://api.weatherapi.com/v1/current.json?key=31014a17aebc42de9e561910233012&q=${location}&aqi=no`)
+        .then( res => res.json())
+        .then(data => updateData(data))
+        .catch( err => {console.log(err)})
+}
+
+// 3. Update data in DOM
+function updateData(obj){
+    // console.log(obj);
+    locationRef.innerText = obj.location.name;
+    temperatureRef.innerText = obj.current.temp_c+" °C";
+    timeRef.innerText = obj.current.last_updated;
+    iconRef.src = obj.current.condition.icon;
+    textRef.innerText = obj.current.condition.text;
+    windspdRef.innerText= obj.current.wind_kph+" kmph";
+    humidityRef.innerText=obj.current.humidity+"%";
+    const isDay = obj.current.is_day;
+        
+    backgroundRef.classList.add('rotate');
+
+    if(isDay===1){
+        backgroundRef.style.backgroundImage='url("assets/day.jpg")';
+        // backgroundRef.classList.add('up');
+    }
+    else{
+        backgroundRef.style.backgroundImage='url("assets/night.jpg")';
+        // backgroundRef.classList.add('down');
+    }
+    
+}
